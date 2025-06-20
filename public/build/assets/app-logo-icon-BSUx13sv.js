878c31d7b08e22b28a05af152acf41cd6f3b2590
4ndres-292 <choquecahuanaandresoriginal@gmail.com>
2025-06-20 10:04:10 -0400

    deploy de events

--- public/build/assets/app-logo-icon-BSUx13sv.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-CSxVKq7s.js";function m(o){return t.jsx("img",{...o,src:"/images/logo_embate.png",alt:"Logo Embate"})}export{m as A};
---
+++ public/build/assets/app-logo-icon-CSlhJcgq.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-BdU4G2fh.js";function m(o){return t.jsx("img",{...o,src:"/images/logo_embate.png",alt:"Logo Embate"})}export{m as A};
