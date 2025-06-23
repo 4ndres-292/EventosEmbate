2021956c96149c225fe2de7298f9f25603e4df87
4ndres-292 <choquecahuanaandresoriginal@gmail.com>
2025-06-23 07:38:38 -0400

    correr build

---
+++ public/build/assets/app-logo-icon-9-k4LoRt.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-DGaXkyY2.js";function e(o){return t.jsx("img",{...o,src:"/storage/logo_embate.png",alt:"Logo Embate"})}export{e as A};
--- public/build/assets/app-logo-icon-CtZCj9JK.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-Q5Uk92vM.js";function e(o){return t.jsx("img",{...o,src:"/storage/logo_embate.png",alt:"Logo Embate"})}export{e as A};
